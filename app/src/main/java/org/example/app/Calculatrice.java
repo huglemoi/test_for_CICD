e5a75ca5b962014acc9c189fc7d92c2b6830255d
Hugo <huglemoi@gmail.com>
2026-08-12 01:46:28 +0200

    Created the structure, and Calculatrice

---
+++ app/src/main/java/org/example/app/Calculatrice.java
@@ -0,0 +1,12 @@
+package org.example.app;
+
+public class Calculatrice {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
+}
